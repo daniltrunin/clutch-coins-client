6d3a1b34f57721e4ee3ba3a7db99eecb9c66a729
daniltrunin <ruslantooyoung@gmail.com>
2025-05-06 23:18:53 +0500

    Исправил баг с padding кнопки

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -6,7 +6,18 @@ const Tasks = () => {
       <h1 className="font-semibold text-sm text-left mt-4 mb-1 text-white/60 pl-2">
         TASKS
       </h1>
-      <Task taskName={"Join our community"} coinsForCompletion={"+100"} buttonText={"Join"} />
+      <div className="flex flex-col gap-2">
+        <Task
+          taskName={"Join our community"}
+          coinsForCompletion={"+100"}
+          buttonText={"Join"}
+        />
+        <Task
+          taskName={"Comment on last post"}
+          coinsForCompletion={"+25"}
+          buttonText={"Comment"}
+        />
+      </div>
     </>
   );
 };
