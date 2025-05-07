be38c489a490fe7932eaef03e83889578577f8a5
daniltrunin <ruslantooyoung@gmail.com>
2025-05-07 16:01:40 +0500

    Добавил иконки монеты на главную

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 
+import icon from "../../assets/coin-icon.png";
+
 const Balance = () => {
   const [coinsAmount] = useState("400");
 
   return (
     <div className="bg-[#2c2c2d] rounded-xl p-4 flex flex-col gap-2">
-      <div className="font-bold text-4xl">{coinsAmount}</div>
+      <div className="font-bold text-4xl flex justify-center gap-2">
+        <img className="w-10 h-10" src={icon} />
+        {coinsAmount}
+      </div>
       <div className="text-white/50">your balance</div>
       <button className="btn-primary">Buy more coins</button>
     </div>
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,10 +1,13 @@
+import icon from "../../assets/coin-icon.png";
+
 const Task = ({ taskName, coinsForCompletion, buttonText }) => {
   return (
     <div className="bg-[#2c2c2d] rounded-xl p-3 flex gap-2 justify-between items-center">
       <div className="flex flex-col">
         <div className="font-bold text-left">{taskName}</div>
-        <div className="font-bold text-white/60 text-left">
+        <div className="font-bold text-white/60 text-left flex items-center gap-1">
           {coinsForCompletion}
+          <img className="w-4 h-4" src={icon}></img>
         </div>
       </div>
       <button className="btn-primary btn-task">{buttonText}</button>
