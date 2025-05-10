e85156d8f35a52c54454763ed7c3c604f7808eaa
daniltrunin <ruslantooyoung@gmail.com>
2025-05-10 16:30:53 +0500

    Изменил дизайн Profile view

--- src/components/ProfileMenu/ProfileMenu.jsx
+++ src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,17 +1,20 @@
 import trade from "../../assets/trade-icon.svg";
 import community from "../../assets/community-icon.svg";
+import arrow from "../../assets/arrow.png";
 
 const ProfileMenu = () => {
   return (
-    <div className="flex flex-col bg-[#2c2c2d] rounded-xl p-4 text-left font-bold gap-4">
-      <div className="flex align-center gap-2 cursor-pointer">
-        <img className="w-6" src={trade} /> Trade Link
+    <div className="bg-[#381011] rounded-2xl p-4 flex flex-col gap-4">
+      <div className="flex align-center gap-2 cursor-pointer font-bold text-md hover:!text-white/85">
+        <img className="w-5" src={trade} /> Ссылка на трейд
+        <img className="w-6 h-6 ml-auto" src={arrow} />
       </div>
       <a
         href="https://t.me/+APBnbq322Hg5YjIy"
-        className="flex align-center gap-2 cursor-pointer !text-white/100"
+        className="flex align-center gap-2 cursor-pointer !text-white/100 hover:!text-white/85 !font-bold text-md"
       >
-        <img className="w-6" src={community} /> Join community
+        <img className="w-5" src={community} /> Подписаться на канал
+        <img className="w-6 h-6 ml-auto" src={arrow} />
       </a>
     </div>
   );
